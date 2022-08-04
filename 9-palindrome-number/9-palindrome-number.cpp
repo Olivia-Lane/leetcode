8814939df01613053a1db6e1b76f7c35d873079b
Oliva-Lane <87322564+Olivia-Lane@users.noreply.github.com>
2022-08-04 22:42:43 +0900

    Time: 8 ms (95.44%), Space: 5.9 MB (31.94%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,79 @@
+#include <cstdint>
+#include <string>
+using std::string;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+
+        string pal = to_string(x);
+        int index = 0;
+        int i = 0;
+        
+        if (pal[0] == '-')
+            return false;
+        
+        // even number
+        if(pal.length() % 2 == 0)
+        {
+            index = pal.length() - 1;
+            i = 0;
+            while(index != (pal.length()/2))
+            {
+                if(pal[i] == pal[index])
+                {
+                    index -= 1;
+                    i += 1;
+                    if(index == pal.length()/2)
+                        break;
+                    else
+                    {
+                        if(i < pal.length())
+                            continue;
+                        else 
+                            break;
+                    }
+                 }
+                else
+                   return false;
+            }   
+            
+            if(i >= pal.length())
+                return -1;
+                            
+            if(pal[index-1]==pal[index])
+                return true;
+            else
+                return false;
+        }
+        
+        // odd number
+        else
+        {
+            index = pal.length() - 1;
+            i = 0;
+            while(index != (pal.length()+1)/2)
+            {
+                if(pal[i] == pal[index])
+                {
+                    index -= 1;
+                    i += 1;
+                    if(i < pal.length())
+                        continue;
+                    else 
+                        break;
+                }
+                else
+                    return false;
+            }
+            
+            if(i >= pal.length())
+                return -1;
+            
+            if(pal[index]==pal[i])
+                return true;
+            else
+                return false;
+        }
+    }
+};
